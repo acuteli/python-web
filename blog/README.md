fee5c4e148e87aa190bbabaf8d6fedfe919584bf
acuteli <congzhouli@163.com>
2017-11-29 14:34:21 +0800

    blog

---
+++ blog/README.md
@@ -0,0 +1,9 @@
+#### 开发环境
+> Django==1.11
+> olefile==0.44
+> Pillow==4.2.1
+> PyMySQL==0.7.11
+> pytz==2017.2
+> virtualenv==15.0.3
+
+执行requirements安装所需要的软件；
--- myobject/myobject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url,include
-from django.contrib import admin
-
-urlpatterns = [
-    url(r'^myadmin/', include('myadmin.urls')),
-    url(r'^', include('myweb.urls')),
-
-]
